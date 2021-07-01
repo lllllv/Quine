c59385e80fb3d90f8faf12e4dc86391a236ab55f
Shinobu <lvxiaoyan1998@hotmail.com>
2021-07-01 13:12:48 +0800

    first commit

---
+++ Quine.py
@@ -0,0 +1,12 @@
+t = "tmp = chr(116) + chr(32) + chr(61) + chr(32) + chr(34)\nlst = t.split(chr(10))\nfor i in lst:\n    tmp = tmp + i\n    tmp = tmp + chr(92) + chr(110)\ntmp = tmp + chr(34)\nprint(tmp)\nprint(t)\n\nWE_CAN_DO_OTHER_CACULATE1=1\nWE_CAN_DO_OTHER_CACULATE2=2"
+tmp = chr(116) + chr(32) + chr(61) + chr(32) + chr(34)
+lst = t.split(chr(10))
+for i in lst:
+    tmp = tmp + i
+    tmp = tmp + chr(92) + chr(110)
+tmp = tmp + chr(34)
+print(tmp)
+print(t)
+
+WE_CAN_DO_OTHER_CACULATE1=1
+WE_CAN_DO_OTHER_CACULATE2=2
